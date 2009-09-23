780ecb3d969e4b39dc1e6086a0cff5aaae66697d
Johan Uhle <johan>
2009-09-23 19:39:17 +0200

    api/tracks/ implemented

--- api.py
+++ api.py
@@ -37,9 +37,13 @@ import models
 import utils
 import settings
 
-def add_to_track_array(track, track_array):
+def add_to_track_array(track, track_array): 
+	"""
+		filling an array with all the track, location and user data per track 
+	"""
 	location_dict = {'lon': track.user.location.location.lon,
 	 								 'lat': track.user.location.location.lat,
+									 'id' : track.user.location.key().id(),
 									 'city': track.user.location.city,
 									 'country': track.user.location.country,
 									 'track_counter': track.user.location.track_counter,
@@ -70,7 +74,7 @@ def add_to_track_array(track, track_array):
 												'duration': track.duration,
 												\
 												'location': location_dict,
-												'user': user_dict})	  			 
+												'user': user_dict})										  			 
 
 def error_response(self, error_name, error_description):
 	"""
@@ -79,96 +83,6 @@ def error_response(self, error_name, error_description):
 	error_response = {'error': True, 'error_name': error_name, 'error_description': error_description}
 	self.response.out.write(json.dumps(error_response))
 	
-class MainHandler(webapp.RequestHandler):
-	"""
-	This script returns JSON-formatted tracks.
-	""" 
-	
-	def get(self): 
-		
-		track_array = [] 
-		split_uri = (self.request.uri).split('/')
-		query_all = "ORDER BY __key__ DESC LIMIT " + str(settings.FRONTEND_TRACKS_LIMIT)
-		has_been_query_for_all = False
-				
-		if re.search("genre", self.request.uri):
-			genre = split_uri[-1]                 
-			query_genre = "WHERE genre IN :1 " + query_all 
-			if genre == 'house': 
-				tracks = models.TrackCache.gql(query_genre, utils.genre_house)
-			elif genre == 'techno': 
-				tracks = models.TrackCache.gql(query_genre, utils.genre_techno)  
-			elif genre == 'dubstep': 
-				tracks = models.TrackCache.gql(query_genre, utils.genre_dubstep)	
-			elif genre == 'hiphop': 
-				tracks = models.TrackCache.gql(query_genre, utils.genre_hiphop)				
-			elif genre == 'electronic': 
-				tracks = models.TrackCache.gql(query_genre, utils.genre_electronic) 
-			elif genre == 'drumandbass': 
-				tracks = models.TrackCache.gql(query_genre, utils.genre_drumandbass)
-			elif genre == 'trance': 
-				tracks = models.TrackCache.gql(query_genre, utils.genre_trance)
-			elif genre == 'rock': 
-				tracks = models.TrackCache.gql(query_genre, utils.genre_rock)
-			elif genre == 'indie': 
-				tracks = models.TrackCache.gql(query_genre, utils.genre_indie)
-			elif genre == 'pop': 
-				tracks = models.TrackCache.gql(query_genre, utils.genre_pop)																				
-			elif genre == 'ambient': 
-				tracks = models.TrackCache.gql(query_genre, utils.genre_ambient)
-			elif genre == 'jazz': 
-				tracks = models.TrackCache.gql(query_genre, utils.genre_jazz)
-			elif genre == 'classical': 
-				tracks = models.TrackCache.gql(query_genre, utils.genre_classical)		
-			else: 
-				tracks = models.TrackCache.gql(query_all) 
-				has_been_query_for_all = True
-				
-			 
-		elif re.search("location", self.request.uri): 
-			# typical location uri is http://localhost:8080/frontend-json/location/-0.1262362/51.5001524/50/
-			lat = split_uri[-4]
-			lng = split_uri[-3]
-			limit = split_uri[-2]
-			tracks = models.TrackCache.gql("WHERE location_lat = :1 AND location_lng = :2 ORDER BY __key__ DESC LIMIT " + limit, lat, lng)   
-			for track in tracks.fetch(int(limit)):
-				self.add_to_track_array(track, track_array, lat + "/" + lng) 
-			tracks_json = json.dumps(track_array)                          
-			self.response.out.write(tracks_json) # finished processing script 
-			return
-			                                                                                     
-		else:
-			tracks = models.TrackCache.gql(query_all)
-			has_been_query_for_all = True   
-			                 
-		used_locations = set()
-		for track in tracks:
-			# a distinct location may only be on the map once because marker on the same position aren't displayed properly
-			location = str(track.location_lat) + "/" + str(track.location_lng)
-			if location in used_locations: continue
-			used_locations.add(location) 																									
-			
-			# keep track of the top cities 
-			if has_been_query_for_all and track.city:
-				try:
-					utils.top_cities.remove(track.city.strip())
-				except ValueError:
-					pass                    
-			self.add_to_track_array(track, track_array, location)
-		
-		#if not all top cities are included, try to add them
-		if has_been_query_for_all: 
-			if utils.top_cities:
-				for city in utils.top_cities:
-					 new_track = models.TrackCache.gql("WHERE city = :1 ORDER BY __key__ DESC LIMIT 1", city).get()
-					 if new_track:
-						  self.add_to_track_array(track, track_array)     
-		 											    
-		tracks_json = json.dumps(track_array)
-		self.response.out.write(tracks_json)
-		return
-
-
 class TracksHandler(webapp.RequestHandler):
 	"""
 		Fetching tracks. Returning json
@@ -178,55 +92,88 @@ class TracksHandler(webapp.RequestHandler):
 		# initializing
 		track_array = []
 		if self.request.get('limit'):
-			limit = self.request.get('limit')
+			limit = int(self.request.get('limit'))
 		else:
-			limit = str(settings.FRONTEND_TRACKS_LIMIT)
-		main_query = 'ORDER BY __key__ DESC LIMIT ' + limit
-		
+			limit = settings.FRONTEND_TRACKS_LIMIT          
+				
 		# Processing for api/tracks/?track_id=track_id
 		if self.request.get('track_id'):
-			track = models.Track.all().filter('track_id', int(self.request.get('track_id'))).get() 
+			track = models.Track.get_by_id(int(self.request.get('track_id')))
 			if track:
 				add_to_track_array(track, track_array)
-				self.response.out.write(track_array)
+				self.response.out.write(json.dumps(track_array))
 			else:
 				error_response(self, 'track_not_found', 'The track with the track_id %s is not in the datastore.' % self.request.get('track_id'))
 			return
 		
-		# Processing for api/tracks/ and api/tracks/?genre=all
-		if self.request.get('genre') == 'all' or \
-			 (not self.request.get('genre') and not self.request.get('location')):
-			tracks = models.Track.gql(main_query)
-			if tracks:													 
-				for track in tracks:
-					add_to_track_array(track, track_array)
-				self.response.out.write(track_array)
-			else:
-				error_response(self, 'no_tracks', 'There are no tracks in the datastore.') 
-			return
-			
 		# Processing for api/tracks/?genre={genre_name} 
-		if self.request.get('genre') and not \
-			 (self.request.get('location') or self.request.get('location_lan') or self.request.get('location_lan')):
+		if self.request.get('genre') and self.request.get('genre') != 'all' and not \
+			 (self.request.get('location') or self.request.get('location_lat') or self.request.get('location_lon')):
 			genre = self.request.get('genre')
 			if genre not in utils.genres:
 				error_response(self, 'unknown_genre', 'Sorry, but we do not know the genre %s.' % genre) 
 				return
 			else:
-				genre_query = 'WHERE genre IN :1 ' + main_query
-				tracks = models.Track.gql(genre_query, utils.genres.get(genre)).fetch(int(limit))
+				tracks = models.Track.all().filter('genre IN', utils.genres.get(genre)).order('-created_at').fetch(limit)
 				if tracks:													 
 					for track in tracks:
 						add_to_track_array(track, track_array)
-					self.response.out.write(track_array)
+					self.response.out.write(json.dumps(track_array))
 				else:
 					error_response(self, 'no_tracks_in_genre', 'There are no tracks of the genre %s in the datastore.' % genre)
 				return					 
 		
 		# Processing for api/tracks/?location=location_id
-		if self.request.get('location'):
-			location = models.Location.all().filter('track_id', int(self.request.get('track_id'))).get() 
-				
+		if self.request.get('location') and not self.request.get('genre'): 
+			location = models.Location.get_by_id(int(self.request.get('location')))
+			if not location:
+				error_response(self, 'location_not_found', 'The location with the id %s is not in the datastore.' % self.request.get('location'))
+			else:
+				users = models.User.all().filter('location', location.key()).fetch(limit)
+				if users:
+					for user in users:                     
+						tracks = models.Track.all().filter('user', user.key()).fetch(limit)
+						for track in tracks:
+							add_to_track_array(track, track_array)
+					self.response.out.write(json.dumps(track_array))
+				else:
+					error_response(self, 'no_tracks_in_location', 'There are no tracks at the location %s in the datastore.' % self.request.get('location'))
+			return
+			
+		# Processing for api/tracks/?location=location_id&genre={genre_name}	
+		if self.request.get('location') and self.request.get('genre'):
+			genre = self.request.get('genre')
+			if genre not in utils.genres:
+				error_response(self, 'unknown_genre', 'Sorry, but we do not know the genre %s.' % genre) 
+				return
+			location = models.Location.get_by_id(int(self.request.get('location')))
+			if not location:
+				error_response(self, 'location_not_found', 'The location with the id %s is not in the datastore.' % self.request.get('location'))
+				return
+			else:
+				users = models.User.all().filter('location', location.key()).fetch(limit)
+				if users:
+					for user in users:                     
+						tracks = models.Track.all().filter('user', user.key()).filter('genre IN', utils.genres.get(genre)).fetch(int(limit))
+						for track in tracks:
+							add_to_track_array(track, track_array)
+					self.response.out.write(json.dumps(track_array))
+				else:
+					error_response(self, 'no_tracks_in_location', 'There are no tracks at the location %s in the datastore.' % self.request.get('location'))
+			return
+			
+		# Processing for api/tracks/ and api/tracks/?genre=all
+		if (self.request.get('genre') == 'all' or not self.request.get('genre')) and not \
+		 	 (self.request.get('location') or self.request.get('location_lat') or self.request.get('location_lon')): 
+			tracks = models.Track.all().order('-created_at').fetch(limit)
+			if tracks:													 
+				for track in tracks:
+					add_to_track_array(track, track_array)
+				self.response.out.write(json.dumps(track_array))
+			else:
+				error_response(self, 'no_tracks', 'There are no tracks in the datastore.') 
+			return			
+			
 class LocationsHandler(webapp.RequestHandler):
 	"""
 		Fetching tracks. Returning json
