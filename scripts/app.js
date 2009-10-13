595ccc73da942905519e5306ebc0d8396be26d22
Johan Uhle <johan>
2009-10-13 12:07:24 +0200

    Updated README

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-/*Copyright (c) 2009 Johan Uhle
+/*Copyright (c) 2009 Johan Uhle, Eric Wahlforss
 
 Permission is hereby granted, free of charge, to any person obtaining
 a copy of this software and associated documentation files (the
